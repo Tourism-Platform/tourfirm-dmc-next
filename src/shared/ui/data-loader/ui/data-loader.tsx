2eaba1cbf9c027b98d7f9e0863f91fb01d572cfc
KDF25 <karimov.damir.faridovich@gmail.com>
2026-08-27 08:48:28 +0500

    fix: svg

--- src/shared/ui/data-loader/ui/data-loader.tsx
+++ src/shared/ui/data-loader/ui/data-loader.tsx
@@ -1,13 +1,12 @@
 import type { FC } from "react";
 
-import mapSrc from "@/shared/assets/map.svg";
 import { cn } from "@/shared/lib";
 
 type TDataLoaderProps = {
 	className?: string;
 };
 
-const mapUrl = typeof mapSrc === "string" ? mapSrc : mapSrc.src;
+const MAP_SRC = "/assets/images/map.svg";
 
 export const DataLoader: FC<TDataLoaderProps> = ({ className }) => {
 	return (
@@ -19,7 +18,7 @@ export const DataLoader: FC<TDataLoaderProps> = ({ className }) => {
 		>
 			{/* eslint-disable-next-line @next/next/no-img-element */}
 			<img
-				src={mapUrl}
+				src={MAP_SRC}
 				alt=""
 				className="h-auto w-[min(90%,18rem)] animate-pulse object-contain sm:w-[min(80%,24rem)] lg:w-[min(70%,32rem)]"
 			/>
